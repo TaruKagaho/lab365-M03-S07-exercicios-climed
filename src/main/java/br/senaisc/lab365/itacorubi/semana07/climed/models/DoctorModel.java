ca80791f65f8ebf17b76749bf8561c7eddb205b7
Adir Sivla Filho <tarukagahobenu@gmail.com>
2023-09-19 09:02:26 -0300

    feat: cria o model de Médico.

--- src/main/java/br/senaisc/lab365/itacorubi/semana07/climed/models/DoctorModel.java
+++ src/main/java/br/senaisc/lab365/itacorubi/semana07/climed/models/DoctorModel.java
@@ -1,4 +1,25 @@
 package br.senaisc.lab365.itacorubi.semana07.climed.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "medicos")
+@Getter
+@Setter
 public class DoctorModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+
+    private String telefone;
+
+    private String email;
+
+    private String crm;
 }
