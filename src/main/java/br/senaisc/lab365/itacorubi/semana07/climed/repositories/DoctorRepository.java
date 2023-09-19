3167658725d64ef8de0949971a97c838f22f466c
Adir Sivla Filho <tarukagahobenu@gmail.com>
2023-09-19 09:02:58 -0300

    feat: cria o repositório de Médico.

--- src/main/java/br/senaisc/lab365/itacorubi/semana07/climed/repositories/DoctorRepository.java
+++ src/main/java/br/senaisc/lab365/itacorubi/semana07/climed/repositories/DoctorRepository.java
@@ -1,4 +1,12 @@
 package br.senaisc.lab365.itacorubi.semana07.climed.repositories;
 
-public interface DoctorRepository {
+import br.senaisc.lab365.itacorubi.semana07.climed.models.DoctorModel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<DoctorModel, UUID> {
 }
